46473189b6b1d7c5841606f159374d8bca9fc6f5
Mark <wowmarkambro@gmail.com>
2020-06-26 19:32:30 -0500

    added form

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,51 +1,96 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 // UI
 import "./App.css";
-import {Button} from 'reactstrap';
+import {Button, Form, FormGroup, Input, Label, FormText} from 'reactstrap';
 // hooks
-import {useSelector,useDispatch} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 // reducer
 import {SmurfReducer} from '../reducers';
 // acions as: actions.getData()
 import * as actions from '../actions';
 
+function App() {
+    const dispatch = useDispatch(SmurfReducer)
+    const Data = useSelector(state => state.data);
 
-function App () {
-  const dispatch = useDispatch(SmurfReducer)
-  const Data = useSelector(state => state.data);
+    // get data
+    const fetchData = () => dispatch(actions.getData());
 
+    const [formData,setFormData] = useState({name: "", age: 0, height: "",id:Date.now()})
 
+    const handleChange = e => {
+        setFormData({...formData,
+            [e.target.name]: e.target.value
+        });
+        console.log(e.target.name, e.target.value)
+    }
 
-  // get data
-  const fetchData = () => dispatch(actions.getData());
+    const renderSmurfs = () => {
+        return Data.map((smurf) => {
+            return (
+                <div key={smurf.id}>
+                    <p>Name: {smurf.name}</p>
+                    <p>Age: {smurf.age}</p>
+                    <p>Height: {smurf.height}</p>
+                </div>
+            )
+        })
+    }
 
-  const renderSmurfs = () =>{
-    return Data.map((smurf)=>{
-      return (
-        <div key={smurf.id}>
-          <p>Name: {smurf.name}</p>
-          <p>Age: {smurf.age}</p>
-          <p>Height: {smurf.height}</p>
-        </div>
-      )
-    })
-  }
-
-  useEffect(()=>{
-    fetchData()
-  },[])
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
-      <div className="App">
-        {/* <Button onClick={()=>{
-          console.log("user asked for data!")
-          fetchData();
-        }}>GET DATA</Button> */}
+        <div className="App">
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                console.log("submitted form!");
+                console.table(formData);
+            }}>
+              <FormText>INPUT NEW SMURF</FormText>
+                <FormGroup>
+                    <Label>Name:
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            placeholder="Name"
+                            value={formData.name}
+                            onChange={handleChange}/>
+                    </Label>
+                </FormGroup>
 
-        {
-        Data.length === 0 ? <p>loading...</p> : renderSmurfs()
-        }
-      </div>
+                <FormGroup>
+                    <Label>Age:
+                        <input
+                            type='number'
+                            name='age'
+                            value={formData.age}
+                            onChange={handleChange}/>
+                    </Label>
+                </FormGroup>
+
+                <FormGroup>
+                    <Label>Height:
+                        <input
+                            type='text'
+                            name='height'
+                            placeholder='3cm'
+                            value={formData.height}
+                            onChange={handleChange}/>
+                    </Label>
+                </FormGroup>
+
+                <Button type='submit' color='primary'>Submit</Button>
+
+            </form>
+
+            {Data.length === 0
+                ? <p>loading...</p>
+                : renderSmurfs()
+}
+        </div>
     );
 }
 
